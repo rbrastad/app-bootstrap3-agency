9b35d4b53132b3c98b5d693452ea203a23e93663
Runar Brastad <runar.brastad@gmail.com>
2016-04-26 21:46:37 +0200

    Clean up and removed unused parts

--- src/main/resources/site/parts/service-details/service-details.js
+++
@@ -1,8 +0,0 @@
-var lib2render = require('/lib/rbrastad/lib2render');
-var app = require('/lib/app.js');
-
-function handleGet(req) {
-    return lib2render.part.renderViewCurrentContent( app.viewServiceDetails );
-}
-
-exports.get = handleGet;
