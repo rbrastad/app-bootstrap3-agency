fe997b88c94a4f1e3dc376aab99ae432850c276d
Runar Brastad <runar.brastad@gmail.com>
2016-04-26 18:00:14 +0200

    Refactored added app.js and moved all view src to app.js from viewSources.js

---
+++ src/main/resources/lib/app.js
@@ -0,0 +1,17 @@
+
+// Views & pages used by the APP
+exports.viewAbout = "/parts/about/about.html";
+exports.viewBanner = "/parts/banner/banner.html";
+exports.viewContact = "/parts/contact/contact.html";
+exports.viewPortfolio = "/parts/portfolio/portfolio.html";
+exports.viewServiceDetails = "/parts/service-details/service-details.html";
+exports.viewService = "/parts/services/services.html";
+exports.viewTeam = "/parts/team/team.html";
+exports.viewClients = "/parts/clients/clients.html";
+
+exports.pageUnStructuredViewer = '/pages/unstructured-viewer/unstructured-viewer.html';
+
+// Used for when part is not configured. uses lib2render default 412 view
+exports.view412 = '/lib/rbrastad/lib2render/html/412.html';
+
+
